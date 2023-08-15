4c266bfcd83a04f9133ea48f876cf524b3a0feca
MahinMuhammad <amimahinurrahman@gmail.com>
2023-08-15 19:38:30 +0600

    Datamodel created

---
+++ PhotoCollection/K.swift
@@ -0,0 +1,12 @@
+//
+//  K.swift
+//  PhotoCollection
+//
+//  Created by Md. Mahinur Rahman on 8/15/23.
+//
+
+import Foundation
+
+struct K{
+    static let persistentContainerTitle = "DataModel"
+}
